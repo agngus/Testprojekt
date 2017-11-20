551e260416a4ef07bc1bd50aa7cfbfdd5cc8c28c
Agneta Gustafsson <agneta.kode@outlook.com>
2017-11-20 19:25:34 +0100

    start

---
+++ Testprojekt/Testprojekt/MyForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Testprojekt
+{
+    class MyForm : Form
+    {
+        public MyForm()
+        {
+            TableLayoutPanel panel = new TableLayoutPanel
+            {
+                RowCount = 3,
+                ColumnCount = 3,
+                Dock = DockStyle.Fill ,
+                BackColor = Color.Gold
+
+            };
+            Controls.Add(panel);
+           
+        }
+    }
+}
--- Testprojekt/Testprojekt/Program.cs
+++ Testprojekt/Testprojekt/Program.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
 
 namespace Testprojekt
 {
     class Program
     {
-        static void Main(string[] args)
+        [STAThread]
+        static void Main()
         {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MyForm());
         }
     }
 }
